af42e19da35f263b5984e36544b79a6204ebf3be
XavierValtierra <xavier.valtierra@icloud.com>
2022-11-16 14:26:54 -0500

    Finished HW 2 up to prompt 10

--- app.js
+++ app.js
@@ -1,2 +1,120 @@
+//Prompt 1
+let myFavoriteNumber;
+myFavoriteNumber = 3;
+  console.log(typeof myFavoriteNumber);
+//Prompt 2
+let myFirstNumber = "Xavier's code";
+  console.log(typeof myFirstNumber);
+let mySecondNumber = true;
+  console.log(typeof mySecondNumber);
+let myThirdNumber = 3;
+  console.log(typeof myThirdNumber);
+//Prompt 3
+let myNumber;
+  console.log(myNumber);
+myNumber = 3;
+  console.log(myNumber);
+myNumber = (myNumber + 10);
+  console.log(myNumber);
+myNumber = (myNumber - 3);
+  console.log(myNumber);
+myNumber = (myNumber * 3);
+  console.log(myNumber);
+myNumber = (myNumber / 7);
+  console.log(myNumber);
+myNumber = (myNumber % 2);
+  console.log(myNumber);
+//Prompt 4
+let firstName = "xavier";
+  console.log(firstName);
+let lastName = "valtierra";
+  console.log(lastName);
+let fullName= firstName + " " + lastName;
+  console.log(fullName);
+//Prompt 5
+let hello = "hello";
+  console.log(hello);
+let world = "world";
+  console.log(world);
+let message = hello + " " + world;
+  console.log(message);
+//Prompt 6
+let sixA = 2 == 3;
+  console.log(sixA);
+let sixB = 2 === 2;
+  console.log(sixB);
+let sixC = 2 !== 1;
+  console.log(sixC);
+let sixD = 3 < 2;
+  console.log(sixD);
+let sixE = 3 > 6;
+  console.log(sixE);
+let sixF = 5 <= 5;
+  console.log(sixF);
+let sixG = 6 >= 7;
+  console.log(sixG);
+//Prompt 7
+let a = 4;
+let b = 53;
+let c = 57;
+let d = 16;
+let e = "Kevin";
 
-console.log("history");
+let sevenA = a < b;
+  console.log(sevenA);
+let sevenB = c > d;
+  console.log(sevenB);
+let sevenC = e === e;
+  console.log(sevenC);
+let sevenD = a < b < c;
+  console.log(sevenD);
+let sevenE = (a === a) < d;
+  console.log(sevenE);
+let sevenF = e === 'Kevin';
+  console.log(sevenF);
+let sevenG = 48 == '48';
+  console.log(sevenG);
+//prompt 8
+const var1 = 6;
+const var2 = 100;
+const var3 = -5;
+const var4 = 3000;
+const var5 = "Jelly Bean";
+
+let eightA = var1 === var2;
+  console.log(eightA);
+let eightB = var3 === var4;
+  console.log(eightB);
+let eightC = 'Peanut' !== 'Peanut';
+  console.log(eightC);
+let eightD = (var1 > var2) < var3;
+  console.log(eightD);
+let eightE = (var1 !== var1) > var4;
+  console.log(eightE);
+let eightF = var5 !== "Jelly Bean";
+  console.log(eightF);
+let eightG = 48 === '48';
+  console.log(eightG);
+//prompt 9
+
+const array1 = ["Marty Marion", "Marty Feldman", "Marty McFly", "Marty Marion"];
+  console.log(array1[2]);
+const array2 = [
+  ["Marty Feldman", "Marty Marion"],
+  ["Marty Stuart", "Marty McFly"],
+  ["Marty Jannetty", "Marty Robbins"],
+];
+  console.log(array2 [1][1]);
+const array3 = [
+  ["Marty Feldman", ["Marty Marion"]],
+  ["Marty Stuart", ["Marty Janetty", ["Marty McFly"], "Marty Robbins"]],
+];
+console.log(array3[1][1][1][0]);
+//Prompt 10
+
+const array4
+= ["a", "b", "c", "d", "e"];
+
+while(i=0, i< 5, i++){
+  console.log(array4)
+}
